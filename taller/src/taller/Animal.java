25e59bd3e47175762eab0b5fd9764636193190b2
YancarlosHuacreCardenas <yancarlos.huacre@vallegrande.edu.pe>
2023-10-18 22:42:48 -0500

    Taller

---
+++ taller/src/taller/Animal.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Yancarlos
+ */
+public class Animal {
+
+    int a;
+    String nombre;
+    boolean v;
+
+    Scanner entrada = new Scanner(System.in);
+
+    public void ingresarDatos() {
+
+        System.out.println("ingresa el numero");
+        a = entrada.nextInt();
+        System.out.println("ingresa el nombre");
+        nombre = entrada.next();
+        System.out.println("ingresar el valor booleano");
+        v = entrada.nextBoolean();
+    }
+
+    public void mostrarDatos() {
+        System.out.println("el numero es " + a
+                + "nel texto es " + nombre
+                + "El valor bolleano es : " + v);
+
+    }
+}
